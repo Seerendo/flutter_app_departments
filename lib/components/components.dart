acb9d2a6ab4146c210767ba615240f3af01504a6
Roddy Andrade <idarkmer@gmail.com>
2022-06-07 20:21:08 -0500

    Banner debug quitado y agregado componente de custom_textformfield

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -1,2 +1,3 @@
+export 'package:app_departments/components/custom_textformfield.dart';
 export 'package:app_departments/components/custom_textbutton.dart';
 export 'package:app_departments/components/custom_appbar.dart';
